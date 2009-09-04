f46edfea3830a990348b0c5365dd70a4e3f64ba7
Sanz <sanz@sanz-laptop.(none)>
2009-09-04 15:41:16 -0400

    Updated url.conf and views Updated url.conf and views

--- firstproject/urls.py
+++ firstproject/urls.py
@@ -1,12 +1,19 @@
 from django.conf.urls.defaults import *
 from django.contrib import admin
-from views import home, blog
+from views import home, YearArchive, MonthArchive, ByDay
 admin.autodiscover()
 
 urlpatterns = patterns('',
-    ('^$', home), # let home display the four most recent posts
+    ('^$', home), # later, this will display the four most recent posts
     (r'^admin/(.*)', admin.site.root),
-    (r'^blog/(?P<year>[\w\._-]+)/$', 'blog', name='year'),
-    (r'^blog/(?P<month>[\w\._-]+)/(?P<year>[\w\._-]+)/$', 'blog', name='month'),
-    (r'^blog/(?P<day>[\w\._-]+)/(?P<year>[\w\._-]+)/(?P<year>[\w\._-]+)/$', 'blog', name='day'),
+    # the patterns below match user input after 'blog/' for a date in the form of
+        # yy/mm/dd
+    (r'^blog/(?P<year>\d{4})/$', 'YearArchive'),
+    (r'^blog/(?P<year>\d{4})/(?P<month>\d{2})/$', 'MonthArchive'),
+    (r'^blog/(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d+)/$', 'ByDay'),
+    #(r'^blog/(?P<year>[\w\._-]+)/$', 'blog', name='year'),
+    #(r'^blog/(?P<year>[\w\._-]+)/(?P<month>[\w\._-]+)/$', 'blog', name='month'),
+    #(r'^blog/(?P<year>[\w\._-]+)/(?P<month>[\w\._-]+)/(?P<day>[\w\._-]+)/$', 'blog', name='day'),
 )
+
+    
--- firstproject/views.py
+++ firstproject/views.py
@@ -6,6 +6,30 @@ def home(request):
         
     return HttpResponse("sup?")
     
-def blog(request, year='$none', month='$none', day='$none'):
-    print "Check!"
-    return render_to_response('sample.html')
+#def blog(request, year='$none', month='$none', day='$none'):
+    #print "Check!"
+    #return render_to_response('sample.html')
+
+#the following three functions catch the incoming requests from the user by
+    # day, month, and year, and pass the variables to a template
+    # the template assumes that it is recieving all of these variables, it does not have a failsafe in place
+def YearArchive(request, year):
+    print "got a year request"
+    return  render_to_response('sample.html', {
+        'year': year,
+    })
+
+def MonthArchive(requst, year, month):
+    print "got a month request"
+    return render_to_response('sample.html', {
+        'year': year,
+        'month': month,
+    })
+
+def ByDay(requst, year, month, day):
+    print "got a by day request"
+    return render_to_response('sample.html', {
+        'year': year,
+        'month': month,
+        'day': day,
+    })
